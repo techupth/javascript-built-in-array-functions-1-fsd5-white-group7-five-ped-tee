ac22ee5bd223742e2c3f92ed09920b56183fae96
pednaruemol <naruemol.kphet@gmail.com>
2023-12-08 16:45:51 +0700

    finished: ex1-5/Built-in-Array-function(HH1)

--- ex-4.js
+++ ex-4.js
@@ -1,3 +1,10 @@
+//Exercise #4 : Get Completed Todo
+// ให้ Declare ตัว Function ชื่อ getCompletedTodo ทำหน้าที่ในการกรอง Todo ที่ทำเสร็จแล้ว (Property completed ที่มี Value เป็น true)
+// Function getCompletedTodo มี Parameter 1 อัน คือ Array ของ Todo ชื่อว่า todos
+// Function อันนี้จะ Return ตัว Array ของ Object ข้อมูล Todo ที่ทำเสร็จแล้ว
+// จากนั้นให้ลอง Execute ตัว Function getCompletedTodo เพื่อทดสอบผลลัพธ์
+// กำหนดให้ใช้ Built-in Array Function (Array.filter) ในการแก้โจทย์ข้อนี้
+
 const todos = [
   { topic: "Doing pre-work", completed: true },
   { topic: "Workout", completed: false },
@@ -6,11 +13,13 @@ const todos = [
   { topic: "Clean the room", completed: true },
 ];
 
-function getCompletedTodo(todos) {
-  // Start coding here
-}
+// function getCompletedTodo(todos) {
+//   // Start coding here
+//   return todos.completed === true;
+// }
 
-getCompletedTodo(todos);
+// const result = todos.filter(getCompletedTodo);
+// console.log(result);
 
 /* 
 	Output:
@@ -20,3 +29,13 @@ getCompletedTodo(todos);
 	  { topic: 'Clean the room', completed: true }
 	]
 */
+
+function getCompletedTodo(todos) {
+  return todos.filter((checkObj) => checkObj.completed === true);
+}
+
+console.log(getCompletedTodo(todos));
+
+//const result = getCompletedTodo(todos);
+//console.log(`Output: ${result}`);
+//console.log(`Output: ${JSON.stringify(result, null, 2)}`);
