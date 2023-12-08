94c9fc28ffa3db71fd662ca8ba15de8c3ba2a0c8
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-08 14:28:07 +0700

    feat: Complete Ex

--- ex-5.js
+++ ex-5.js
@@ -5,8 +5,18 @@ const students = [
   { name: "Joan", score: 95 },
 ];
 
-function getAverageStudentScore(students) {
-  // Start coding here
+function getAverageStudentScore(accumulator,currentValue) {
+  // let totalScore = 0
+  // let AverageScore = 0 
+  // for (let i = 0 ; i < students.length ; i++){
+  //   totalScore = students[i].score + totalScore
+  //   AverageScore = totalScore/(i+1)
+  // }
+  // console.log(AverageScore)
+  // return AverageScore
+  accumulator = accumulator+currentValue.score
+  return accumulator
 }
+const result = students.reduce(getAverageStudentScore,0)/students.length
+console.log(result)// Output: 87.5
 
-getAverageStudentScore(students); // Output: 87.5
